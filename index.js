3071467292afc4b47d8e50e23960a4cfcfccca7a
kingoftg428 <holdjosh1@gmail.com>
2025-02-27 14:09:31 +0100

    first commit

--- index.js
+++ index.js
@@ -2,5 +2,7 @@ function SendMail(){
     var params = {
         message: document.getElementById("message").value,
     }
-    emailjs.send("service_9kqi0uh","template_nwo10w6", params);
+    emailjs.send("service_9kqi0uh","template_nwo10w6", params).then(document.write("<h1>An Error Occurred During the Connection Please Try Again.</h1>")
+        
+    );
 }

